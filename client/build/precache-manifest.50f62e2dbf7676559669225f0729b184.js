e8c19ab83d384db608be8fea509df8df3df06d63
vasavi245 <r.vasavi04@gmail.com>
2021-01-29 18:18:28 +0800

    build

---
+++ client/build/precache-manifest.50f62e2dbf7676559669225f0729b184.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e27d8d8a6b7b35939ff",
+    "url": "/static/css/main.a295c08d.chunk.css"
+  },
+  {
+    "revision": "1e27d8d8a6b7b35939ff",
+    "url": "/static/js/main.d6db0a9f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0bedf1b42555cb6b75ef",
+    "url": "/static/js/2.7885ef80.chunk.js"
+  },
+  {
+    "revision": "b25ea6809bbeec52f6bdf56fc6ba25b4",
+    "url": "/index.html"
+  }
+];
