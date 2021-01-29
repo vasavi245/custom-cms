5868b1db86e19fe6835163f8f1b7c8d6ca0da6b0
vasavi245 <r.vasavi04@gmail.com>
2021-01-29 17:58:59 +0800

    server file changed

--- server.js
+++ server.js
@@ -12,12 +12,14 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-app.use(routes);
+
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
 console.log(process.env.MONGODB_URI);
+
+// Add routes, both API and view
+app.use(routes);
 
 // Start the API server
 app.listen(PORT, function() {
